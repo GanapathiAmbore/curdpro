c477028ebca7b0f369b5bfb34247492bd45d0fd7
suddashankar <shankardwaraka@gmail.com>
2020-12-26 15:51:15 +0530

    added urls

--- curdpro/urls.py
+++ curdpro/urls.py
@@ -19,5 +19,6 @@ from curdapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('index/',views.index,name='index')
 ]
