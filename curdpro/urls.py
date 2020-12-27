77e43b25364bd1f16f5df88681e4f57842470a97
suddashankar <shankardwaraka@gmail.com>
2020-12-27 08:56:42 +0530

    added contact

--- curdpro/urls.py
+++ curdpro/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
     path('index/',views.index,name='index'),
-    path('about/',views.about,name='about')
+    path('about/',views.about,name='about'),
+    path('contact/',views.contact,name='contact')
 ]
